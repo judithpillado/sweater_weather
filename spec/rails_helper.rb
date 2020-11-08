d65bdbeae93b852d8a15cf3759cbdaaf0266692b
Judith Pillado <judith.pillado@gmail.com>
2020-11-07 23:44:37 -0600

    Add enviroment variables

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -75,7 +75,8 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  config.filter_sensitive_data('<api_key>') { ENV['api_key'] }
+  config.filter_sensitive_data('MAPQUEST_API') { ENV['MAPQUEST_API'] }
+  config.filter_sensitive_data('WEATHER_API') { ENV['WEATHER_API'] }
   config.default_cassette_options = { re_record_interval: 7.days }
   config.configure_rspec_metadata!
 end
