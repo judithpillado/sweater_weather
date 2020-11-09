d42e4f9df15d924a684dbbf9d1d9fe90aea4f2f2
Judith Pillado <judith.pillado@gmail.com>
2020-11-09 13:02:43 -0600

    Create method to find distance between trails

--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -7,6 +7,15 @@ class MapquestService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def self.distance_to_trail(location, trail_location)
+    response = conn.get('/directions/v2/route') do |req|
+      req.params[:key] = ENV['MAPQUEST_API']
+      req.params[:from] = location
+      req.params[:to] = trail_location[:trails][0][:location]
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def self.conn
     Faraday.new("http://www.mapquestapi.com/")
   end
