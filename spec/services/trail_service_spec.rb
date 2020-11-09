0c92b43c7b717f53a5cabbf9a396f3b899810663
Judith Pillado <judith.pillado@gmail.com>
2020-11-09 10:17:23 -0600

    Create Trail Service spec

---
+++ spec/services/trail_service_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe TrailService, :vcr do
+  describe 'class methods' do
+    it 'fetch_trail' do
+
+      coordinates = { lat: 39.738453, lng: -104.984853 }
+
+      json = TrailService.fetch_trail(coordinates)
+
+      expect(json).to be_a(Hash)
+    end
+  end
+end
