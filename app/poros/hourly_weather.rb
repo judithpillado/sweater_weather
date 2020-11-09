b9cee39529439704d7b7c703ecc9b398b05e2acc
Judith Pillado <judith.pillado@gmail.com>
2020-11-08 18:57:47 -0600

    Create Hourly Weather PORO with respective test

---
+++ app/poros/hourly_weather.rb
@@ -0,0 +1,18 @@
+class HourlyWeather
+  attr_reader :time, :temperature, :wind_speed, :wind_direction, :conditions, :icon
+
+  def initialize(attr)
+    @time = Time.at(attr[:dt])
+    @temperature = attr[:temp]
+    @wind_speed = attr[:wind_speed]
+    @wind_direction = attr[:wind_deg]
+    @conditions = attr[:weather][0][:description]
+    @icon = attr[:weather][0][:icon]
+  end
+
+  def self.eight_hours(weather)
+    weather.take(8).map do |per_hour|
+     HourlyWeather.new(per_hour)
+   end
+  end
+end
