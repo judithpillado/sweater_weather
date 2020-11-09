a999775bb95b48fc61b49fa6cf9477f1e15765ad
Judith Pillado <judith.pillado@gmail.com>
2020-11-09 13:03:39 -0600

    Create Trail PORO

---
+++ app/poros/trail.rb
@@ -0,0 +1,24 @@
+class Trail
+  attr_reader :id,
+              :location,
+              :forecast,
+              :trails
+
+  def initialize(trail, location, weather, commute)
+    @id = nil
+    @location = location
+    @forecast = {
+      summary: weather[:current][:weather][0][:description],
+      temperature: weather[:current][:temp]
+    }
+    @trails = [
+      {
+        name: trail[:trails][0][:name],
+        summary: trail[:trails][0][:summary],
+        difficulty: trail[:trails][0][:difficulty],
+        location: trail[:trails][0][:location],
+        distance_to_trail: commute
+      }
+    ]
+  end
+end
