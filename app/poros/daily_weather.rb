4793b007e3a58cf1333ff5d1337cf9e6d8b223b1
Judith Pillado <judith.pillado@gmail.com>
2020-11-08 18:58:16 -0600

    Create Daily Weather PORO with respective test

---
+++ app/poros/daily_weather.rb
@@ -0,0 +1,19 @@
+class DailyWeather
+  attr_reader :date, :sunrise, :sunset, :max_temp, :min_temp, :conditions, :icon
+
+  def initialize(attr)
+    @date = Time.at(attr[:dt])
+    @sunrise = Time.at(attr[:sunrise])
+    @sunset = Time.at(attr[:sunset])
+    @max_temp = attr[:temp][:max]
+    @min_temp = attr[:temp][:min]
+    @conditions = attr[:weather][0][:description]
+    @icon = attr[:weather][0][:icon]
+  end
+
+  def self.five_days(weather)
+    weather.take(5).map do |per_day|
+      HourlyWeather.new(per_day)
+    end
+  end
+end
