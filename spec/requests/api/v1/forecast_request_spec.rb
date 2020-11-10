d7186bff32136d27816c22e94227109d34ad5168
Judith Pillado <judith.pillado@gmail.com>
2020-11-09 18:34:40 -0600

    Fix json response

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -6,7 +6,8 @@ describe 'Forecast API' do
 
     json = JSON.parse(response.body, symbolize_names: true)
     expect(response).to be_successful
-    expect(json[:data][:attributes][:location][:results][0][:providedLocation][:location]).to eq("denver,co")
+    
+    expect(json[:data][:attributes][:location]).to eq("denver,co")
     expect(json[:data][:attributes][:current]).to be_a(Hash)
     expect(json[:data][:attributes][:current][:datetime]).to be_a(String)
     expect(json[:data][:attributes][:daily]).to be_an(Array)
