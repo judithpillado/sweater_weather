1afdee9d429257759407100d706080e7c70c7e55
Judith Pillado <judith.pillado@gmail.com>
2020-11-10 14:59:25 -0600

    Create method to find distance between two destinations with spec

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe MapquestService, :vcr do
       location = 'denver,co'
 
       json = MapquestService.fetch_coordinates(location)
-      
+
       expect(json).to be_a(Hash)
 
       expect(json).to have_key :lat
@@ -15,6 +15,24 @@ RSpec.describe MapquestService, :vcr do
 
       expect(json).to have_key :lng
       expect(json[:lng]).to be_a(Float)
+    end
+
+    it 'fetch_distance_between' do
+      origin = 'denver,co'
+      destination = 'chicago,il'
+
+      route = MapquestService.fetch_distance_between(origin, destination)
+
+      expect(route).to be_a(Hash)
+      expect(route[:route]).to be_a(Hash)
+
+      expect(route[:route]).to have_key :legs
+      expect(route[:route][:legs]).to be_an(Array)
+
+      route_info = route[:route][:legs][0]
+
+      expect(route_info).to be_a(Hash)
+      expect(route_info[:hasHighway]).to eq(true)
     end
   end
 end
