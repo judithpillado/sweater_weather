be62cfe0dbe24dc597ab618b67e88e5a0a8d5679
Judith Pillado <judith.pillado@gmail.com>
2020-11-09 18:37:23 -0600

    Update Facade with spec - REFACTOR

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,9 +1,7 @@
 class ForecastFacade
-  def self.fetch_forecast(city)
-    location = MapquestService.fetch_location(city)
-    latitude = location[:results][0][:locations][0][:latLng][:lat]
-    longitude = location[:results][0][:locations][0][:latLng][:lng]
-    weather = WeatherService.fetch_weather(latitude, longitude)
-    Forecast.new(location, weather)
+  def self.fetch_forecast(location)
+    coordinates = MapquestService.fetch_coordinates(location)
+    weather_data = WeatherService.fetch_weather(coordinates)
+    Forecast.new(location, weather_data)
   end
 end
