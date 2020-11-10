2af28e9c07e73219709414fa94636bbb7fa87e33
Judith Pillado <judith.pillado@gmail.com>
2020-11-09 19:08:10 -0600

    Image PORO spec passing

--- spec/poros/image_spec.rb
+++ spec/poros/image_spec.rb
@@ -1,18 +1,16 @@
-# require 'rails_helper'
-#
-# RSpec.describe Image do
-#   it 'exists' do
-#     background_data = {
-#             :id=>nil,
-#             :url=>"https://www.pexels.com/photo/photo-of-people-holding-each-other-s-hands-3184423/"
-#             }
-#     location = 'denver,co'
-#     image = Image.new(background_data, location)
-#
-#     expect(image).to be_an(Image)
-#     expect(image.id).to eq(nil)
-#     expect(image.location).to eq(location)
-#     expect(image.image_url).to eq('https://www.pexels.com/photo/photo-of-people-holding-each-other-s-hands-3184423/')
-#     expect(image.credit[:photographer]).to eq('fauxels')
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe Image do
+  it 'exists' do
+    background_data = JSON.parse(File.read('spec/fixtures/json/image_data.json'), symbolize_names: true)
+    location = 'denver,co'
+    image = Image.new(background_data, location)
+    
+    expect(image).to be_an(Image)
+    expect(image.id).to eq(nil)
+    expect(image.location).to eq(location)
+    expect(image.image_url).to be_a(String)
+    expect(image.credit[:source]).to eq('pexels.com')
+    expect(image.credit[:photographer]).to be_a(String)
+  end
+end
