85ca6e72b7ba7000ad6af305ab5c6f98db717183
Judith Pillado <judith.pillado@gmail.com>
2020-11-10 20:16:20 -0600

    Update weather service to include new method

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -9,6 +9,16 @@ class WeatherService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def self.fetch_destination_forecast(route_info)
+    response = conn.get("data/2.5/onecall") do |req|
+      req.params[:lat] = route_info[:route][:locations].last[:latLng][:lat]
+      req.params[:lon] = route_info[:route][:locations].last[:latLng][:lng]
+      req.params[:exclude] = "minutely"
+      req.params[:units] = "imperial"
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def self.conn
     Faraday.new("http://api.openweathermap.org/") do |req|
       req.params[:appid] = ENV['WEATHER_API']
